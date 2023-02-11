0c2ab2163dc3a0d920f30285ac94634e78ed7147
Armen23023 <mirzoyana805@gmail.com>
2023-02-11 19:28:02 +0400

    WIP

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,22 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<Integer>();
+
+        arrayDeque.add(firstQueue.poll());
+        arrayDeque.add(firstQueue.poll());
+        arrayDeque.add(secondQueue.poll());
+        arrayDeque.add(secondQueue.poll());
+
+        while (firstQueue.size()!=0 || secondQueue.size() !=0){
+            firstQueue.add(arrayDeque.pollLast());
+            arrayDeque.add(firstQueue.poll());
+            arrayDeque.add(firstQueue.poll());
+            secondQueue.add(arrayDeque.pollLast());
+            arrayDeque.add(secondQueue.poll());
+            arrayDeque.add(secondQueue.poll());
+        }
+        return arrayDeque;
+
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,25 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        Deque<Integer> queue = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+        int i = 1;
+        while (!queue.isEmpty()){
+            if (i == everyDishNumberToEat){
+                list.add(queue.pollFirst());
+                i=1;
+            }else {
+                Integer integer = queue.pollFirst();
+                queue.addLast(integer);
+                i++;
+            }
+        }
+        return list;
     }
 }
